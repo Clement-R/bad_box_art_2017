e3f0fb027ac3df988f4a7351fcb576e86525bc90
Nujerk <junkerlucas@gmail.com>
2017-03-26 18:43:38 +0200

    Camera follow player

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float allowedDistance = 32f;
+    private GameObject _player;
+
+    void Start() {
+        _player = GameManager.Instance.player;
+    }
+
+    void LateUpdate() {
+        var current = gameObject.transform.position;
+        var playerPos = _player.transform.position;
+        var delta = playerPos.x - current.x;
+        Debug.Log(delta);
+        if(Mathf.Abs(delta) > allowedDistance) {
+            current.x = _player.transform.position.x - (Mathf.Sign(delta) * allowedDistance);
+            gameObject.transform.position = current;
+        }
+    }
+}
